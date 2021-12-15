674d31085a7cf431b62dac602baa3b042fcc2c51
bettyvng <betty.vg8@gmail.com>
2021-12-15 05:07:28 +0000

    Implement character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+  $("#tweet-text").on("input", function() {
+    const counter = 140 - (this.value || '').length;
+    const jCounter = $(".counter");
+    if (counter < 0) {
+      jCounter.addClass("counter-negative");
+    } else {
+      jCounter.removeClass("counter-negative");
+    }
+    jCounter.html(counter);
+  })
+});
