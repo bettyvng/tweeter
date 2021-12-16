75a69f46dbeeb8eba4934824fe2a0810d29436e1
Betty Vuong <bettyvng@BZ-macbook.local>
2021-12-15 21:46:02 -0500

    Fetching tweets with Ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,33 +1,10 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
 
+/**
+ * 
+ * Creat tweet element that can be rendered
+ * @param {*} tweet - tweet object
+ * @returns tweet element to be rendered
+ */
 const createTweetElement = function(tweet) {
   const $tweetElementMarkup = `
     <article class="tweet">
@@ -54,6 +31,10 @@ const createTweetElement = function(tweet) {
   return $tweetElementMarkup;
 }
 
+/**
+ * Render a list of tweets on our UI
+ * @param {*} tweets - a list of tweets to be rendered
+ */
 const renderTweets = function(tweets) {
   // loops through tweets
   // calls createTweetElement for each tweet
@@ -63,6 +44,19 @@ const renderTweets = function(tweets) {
   });
 }
 
+/**
+ * Load tweets from server
+ */
+const loadTweets = function() {
+  // Load tweets from server
+  $.get("http://localhost:8080/tweets", function(result) {
+    renderTweets(result);
+  });
+}
+
+/**
+ * Reset new tweet area; empty textarea and reset tweet counter
+ */
 const resetNewTweetArea = function() {
   // reset textarea input value
   $('#tweet-text').val('');
@@ -72,6 +66,9 @@ const resetNewTweetArea = function() {
   jCounter.html(140);
 }
 
+/**
+ * Initialize add-tweet handler so that it can post on submit
+ */
 const initAddTweetHandler = function() {
   // Listen to tweet form submission
   $( "#new-tweet-form" ).submit(function( event ) {
@@ -87,7 +84,7 @@ const initAddTweetHandler = function() {
 const bootstrap = function() {
   // Wait for document to be ready
   $(document).ready(function() {
-    renderTweets(data);
+    loadTweets();
     initAddTweetHandler();
   });
 }
